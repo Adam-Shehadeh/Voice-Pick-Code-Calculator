41e0172c92ba9260a6edd3449793735b0e84f394
Adam-Shehadeh <adam.m.shehadeh@gmail.com>
2018-03-13 12:35:02 -0500

    Initial Commit

---
+++ Crc16_Lib_App/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Crc16_Lib_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbGTIN = new System.Windows.Forms.TextBox();
+            this.tbCrcResults = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gitHubToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sourceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gS1DocumentationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(193, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbGTIN
+            // 
+            this.tbGTIN.Location = new System.Drawing.Point(58, 74);
+            this.tbGTIN.MaxLength = 999;
+            this.tbGTIN.Name = "tbGTIN";
+            this.tbGTIN.Size = new System.Drawing.Size(193, 20);
+            this.tbGTIN.TabIndex = 1;
+            // 
+            // tbCrcResults
+            // 
+            this.tbCrcResults.Location = new System.Drawing.Point(58, 100);
+            this.tbCrcResults.Name = "tbCrcResults";
+            this.tbCrcResults.Size = new System.Drawing.Size(193, 20);
+            this.tbCrcResults.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(273, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gitHubToolStripMenuItem,
+            this.sourceToolStripMenuItem,
+            this.gS1DocumentationToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // gitHubToolStripMenuItem
+            // 
+            this.gitHubToolStripMenuItem.Name = "gitHubToolStripMenuItem";
+            this.gitHubToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.gitHubToolStripMenuItem.Text = "GitHub";
+            this.gitHubToolStripMenuItem.Click += new System.EventHandler(this.gitHubToolStripMenuItem_Click);
+            // 
+            // sourceToolStripMenuItem
+            // 
+            this.sourceToolStripMenuItem.Name = "sourceToolStripMenuItem";
+            this.sourceToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sourceToolStripMenuItem.Text = "Source";
+            this.sourceToolStripMenuItem.Click += new System.EventHandler(this.sourceToolStripMenuItem_Click);
+            // 
+            // gS1DocumentationToolStripMenuItem
+            // 
+            this.gS1DocumentationToolStripMenuItem.Name = "gS1DocumentationToolStripMenuItem";
+            this.gS1DocumentationToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.gS1DocumentationToolStripMenuItem.Text = "GS1 Documentation";
+            this.gS1DocumentationToolStripMenuItem.Click += new System.EventHandler(this.gS1DocumentationToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "VoicePick Code Generator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "GTIN:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Result:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 175);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbCrcResults);
+            this.Controls.Add(this.tbGTIN);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Voice Pick";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbGTIN;
+        private System.Windows.Forms.TextBox tbCrcResults;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gitHubToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sourceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gS1DocumentationToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Crc16_Lib_App/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SunCRC.CRCGeneric;
+using SunCRC.CrcTest;
+using SunCRC.CrcTest.Crc8Test;
+using SunCRC.CrcTest.Crc16Test;
+using SunCRC.CrcTest.Crc32Test;
+using SunCRC.CrcTest.Crc64Test;
+using SunCRC.CrcGeneric;
+using SunCRC.CRCGeneric.BaseGeneric;
+using SunCRC.Crc.Crc8;
+using SunCRC.Crc.Crc16;
+using SunCRC.Crc;
+using System.Diagnostics;
+
+
+namespace Crc16_Lib_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string newgtin = "00097111974689180143180313";
+            tbGTIN.Text = newgtin;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                ushort ResultCrc;
+                string value = tbGTIN.Text;
+                byte[] a = new byte[value.Length];
+                char[] b = new char[value.Length];
+                b = value.ToCharArray();
+                byte [] GtinBytes = new byte[value.Length];
+
+
+            System.Collections.IEnumerable collection = Enumerable.Range(100, 10);
+
+            foreach (var ch in b.OfType<object>().Select((x, i) => new { x, i }))
+            {
+                int index = ch.i;
+                try
+                {
+                        byte _GtinBytes = Convert.ToByte(ch.x);
+                        GtinBytes[index] = _GtinBytes;
+
+                }
+                
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Unable to convert u+{0} to a byte.",
+                                      Convert.ToInt16(ch).ToString("X4"));
+                }
+
+            }
+
+                Crc16Model getVPCModel = new Crc16Model(0x8005, 0x0, 0x0, true, true);
+                Crc16 getVPCDB = new Crc16(getVPCModel);
+                ResultCrc = getVPCDB.Compute(GtinBytes);
+            tbCrcResults.Text = ResultCrc.ToString();
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        private void gitHubToolStripMenuItem_Click(object sender, EventArgs e) {
+            Process.Start("https://github.com/Adam-Shehadeh/Voice-Pick-Code-Calculator");
+        }
+
+        private void sourceToolStripMenuItem_Click(object sender, EventArgs e) {
+            Process.Start("https://www.producetraceability.org/resources/voicecode");
+        }
+
+        private void gS1DocumentationToolStripMenuItem_Click(object sender, EventArgs e) {
+            Process.Start("https://www.gs1.org/docs/barcodes/GS1_General_Specifications.pdf");
+        }
+    }
+}
